98aa4374c8b8a38f2f75e1f0ba4322cbc9a5c079
DTing <dennisting@gmail.com>
2016-08-06 11:02:30 -0700

    Fix lint error in user.model.js (#8)

--- api/user/user.model.js
+++ api/user/user.model.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
   username: { type: String, required: true, index: { unique: true } },
   password: { type: String, required: true },
 });
 
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
+module.exports = mongoose.model('User', userSchema);
