c6e8f7cc15a4b81bc4d58c804d40c4a994a6106f
Dennis Ting <dennisting@gmail.com>
2016-08-06 09:48:50 -0700

    Add utility modules

--- config/express.js
+++ config/express.js
@@ -1,11 +1,17 @@
-const parser = require('body-parser');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const compression = require('compression');
+const helmet = require('helmet');
 const morgan = require('morgan');
 const environment = require('./environment');
 
-module.exports = app => {
+module.exports = function config(app) {
   app.set('port', environment.port);
   app.set('ip', environment.ip);
   app.use(morgan('dev'));
-  app.use(parser.json());
-  app.use(parser.urlencoded({ extended: true }));
+  app.use(helmet());
+  app.use(cors());
+  app.use(compression());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
 };
