fb2dbeffbd0a9b1e4c257637207c1c441576f950
Dennis Ting <dennisting@gmail.com>
2016-08-08 18:30:26 -0700

    Fix server to listen correctly

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
 const express = require('express');
-const Promise = require('bluebird');
 const configExpress = require('./config/express');
 
 const app = express();
 
 configExpress(app);
 
-const listen = () => Promise.promisify(app.listen, app)(app.get('port'), app.get('ip'));
-
-module.exports = {
-  app,
-  listen,
-};
+module.exports = app;
