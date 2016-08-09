0149ff17b19201b29754dc89892a1c06b1c01903
Omneet Lachhar <omneetlachhar@gmail.com>
2016-08-08 18:43:08 -0700

    Implement local authentication for login and signup w/ webtoken

--- api/user/user.model.js
+++ api/user/user.model.js
@@ -1,8 +1,24 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+const Promise = require('bluebird');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, index: { unique: true } },
   password: { type: String, required: true },
 });
 
-module.exports = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
+const cipher = Promise.promisify(bcrypt.hash);
+const compare = Promise.promisify(bcrypt.compare);
+
+User.comparePassword = (candidatePw, savedPw) => compare(candidatePw, savedPw);
+
+userSchema.pre('save', function (next) {
+  cipher(this.password, null, null).bind(this)
+    .then(hash => {
+      this.password = hash;
+      next();
+    });
+});
+
+module.exports = User;
