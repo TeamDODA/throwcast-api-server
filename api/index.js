ebaa9bf97a5fce7f2924a3ac4809e8b70aef1613
DTing <dennisting@gmail.com>
2016-08-10 17:00:03 -0700

    Podcast model and tests (#23)
    Add utils module for tests.
    Refactor tests to be more uniform.

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+
+const podcast = require('./podcast');
 const station = require('./station');
 
 const router = express.Router();
 
+router.use('/podcasts', podcast);
 router.use('/stations', station);
 
 module.exports = router;
---
+++ api/podcast/podcast.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const oid = mongoose.Schema.Types.ObjectId;
+
+const podcastSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  link: { type: String, required: true },
+  description: { type: String, required: true },
+  pubDate: { type: Date },
+  imageUrl: { type: String },
+  station: { type: oid, required: true, ref: 'Station' },
+});
+
+const Podcast = mongoose.model('Podcast', podcastSchema);
+
+module.exports = Podcast;
