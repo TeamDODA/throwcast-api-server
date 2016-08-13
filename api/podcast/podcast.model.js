a98a0653c65af7720893c2ffa07fd189c024067d
Omneet Lachhar <omneetlachhar@gmail.com>
2016-08-12 18:53:34 -0700

    Create seed podcast/station data for database

--- api/podcast/podcast.model.js
+++ api/podcast/podcast.model.js
@@ -4,7 +4,7 @@ const oid = mongoose.Schema.Types.ObjectId;
 
 const podcastSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  link: { type: String, required: true },
+  link: { type: String, required: true, unique: true },
   description: { type: String, required: true },
   pubDate: { type: Date },
   imageUrl: { type: String },
--- api/station/station.model.js
+++ api/station/station.model.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const stationSchema = new mongoose.Schema({
-  title: { type: String, required: true },
+  title: { type: String, required: true, unique: true },
   link: { type: String, required: true },
   description: { type: String, required: true },
   imageUrl: { type: String },
