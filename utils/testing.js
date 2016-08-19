71dc2b5e558575181d3df0939f3e426d3431a513
Dennis Ting <dennisting@gmail.com>
2016-08-19 15:49:05 -0700

    Use util handlers for podcast and station controllers

--- utils/testing.js
+++ utils/testing.js
@@ -3,7 +3,13 @@ const Station = require('../api/station/station.model');
 const User = require('../api/user/user.model');
 const Playlist = require('../api/playlist/playlist.model');
 
-module.exports.cleanModels = () => Podcast.remove()
-  .then(() => Station.remove())
-  .then(() => User.remove())
-  .then(() => Playlist.remove());
+module.exports.cleanModels = function cleanModels() {
+  return Podcast.remove()
+    .then(() => Station.remove())
+    .then(() => User.remove())
+    .then(() => Playlist.remove());
+};
+
+module.exports.entitiesToIds = function entitiesToIds(entities) {
+  return entities.map(entity => entity._id);
+};
