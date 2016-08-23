1c5aefa2ace0dd0dc16af4309d726387bba29394
DTing <dennisting@gmail.com>
2016-08-23 15:20:34 -0700

    Add aggregation for popular stations (#71)

--- api/station/station.controller.js
+++ api/station/station.controller.js
@@ -1,16 +1,33 @@
 const u = require('../../utils');
 const Station = require('./station.model');
 const Podcast = require('../podcast/podcast.model');
+const User = require('../user/user.model');
 
 const controller = {};
 
-controller.list = (req, res) => {
+controller.list = function list(req, res) {
   Station.find({}).exec()
     .then(u.respondWithResult(res))
     .catch(u.handleError(res));
 };
 
-controller.listPodcasts = (req, res) => {
+controller.popular = function popular(req, res) {
+  const pipeline = [];
+  pipeline.push({ $project: { subscriptions: 1 } });
+  pipeline.push({ $unwind: '$subscriptions' });
+  pipeline.push({ $group: { _id: '$subscriptions', count: { $sum: 1 } } });
+  pipeline.push({ $sort: { count: -1, _id: 1 } });
+  pipeline.push({
+    $lookup: { from: 'stations', localField: '_id', foreignField: '_id', as: 'station' },
+  });
+  pipeline.push({ $unwind: '$station' });
+  pipeline.push({ $project: { _id: 0, station: 1, count: 1 } });
+  User.aggregate(pipeline)
+    .then(results => res.json(results))
+    .catch(u.handleError(res));
+};
+
+controller.listPodcasts = function listPodcasts(req, res) {
   Podcast.find({ station: req.params.stationId }).exec()
     .then(u.respondWithResult(res))
     .catch(u.handleError(res));
