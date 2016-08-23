e014d610e0f92359356a76c91fcf62ebf83a919f
DTing <dennisting@gmail.com>
2016-08-23 01:09:29 -0700

    Update podcast schema (#69)

--- api/podcast/podcast.model.js
+++ api/podcast/podcast.model.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const oid = mongoose.Schema.Types.ObjectId;
 
 const podcastSchema = new mongoose.Schema({
-  station: { type: oid, required: true, ref: 'Station', index: true },
-  guid: { type: String, required: true, index: true },
+  guid: { type: String, required: true },
   title: { type: String, required: true },
   published: Date,
   duration: Number,
@@ -12,9 +11,12 @@ const podcastSchema = new mongoose.Schema({
   description: String,
   enclosure: {},
   image: String,
+  station: { type: oid, required: true, ref: 'Station', index: true },
 }, {
   timestamps: true,
 });
+
+podcastSchema.index({ station: 1, guid: 1 }, { unique: true });
 
 const Podcast = mongoose.model('Podcast', podcastSchema);
 
