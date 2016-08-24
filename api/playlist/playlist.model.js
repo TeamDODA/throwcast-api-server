523dfda94534dad8c73c72a5e27fed26b8b982cb
Dennis Ting <dennisting@gmail.com>
2016-08-24 09:14:09 -0700

    Specify title to be indexed on elastic search

--- api/playlist/playlist.model.js
+++ api/playlist/playlist.model.js
@@ -4,7 +4,7 @@ const mongoosastic = require('mongoosastic');
 const oid = mongoose.Schema.Types.ObjectId;
 
 const playlistSchema = new mongoose.Schema({
-  title: { type: String, required: true },
+  title: { type: String, required: true, es_indexed: true },
   owner: { type: oid, required: true, ref: 'User' },
   podcasts: [{ type: oid, ref: 'Podcast' }],
 }, {
