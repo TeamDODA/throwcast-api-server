857361b271b6b5cb9b9fe29a000dffb9b1002d21
DTing <dennisting@gmail.com>
2016-08-24 18:11:39 -0700

    Fix user playlists (#78)

--- api/user/playlist/playlist.controller.js
+++ api/user/playlist/playlist.controller.js
@@ -4,7 +4,7 @@ const Playlist = require('../../playlist/playlist.model');
 const controller = {};
 
 controller.list = function create(req, res) {
-  Playlist.find({ owner: req.owner._id })
+  Playlist.find({ owner: req.user._id })
     .then(u.respondWithResult(res))
     .catch(u.handleError(res));
 };
